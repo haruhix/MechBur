dc670fda3f520f50245b6ec4e046cdfa3f90fb04
Sayan Boloshkinov <146525643+Spacqman@users.noreply.github.com>
2025-01-17 05:25:50 +0900

    Initial commit

---
+++ Source/MechBur/MechBur.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MechBur.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MechBur, "MechBur" );
